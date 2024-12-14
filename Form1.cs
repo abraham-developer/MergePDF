ec258cceb7c62c46ba4123507ef780ca977ce0a4
Abraham-developer <abraham-program@outlook.com>
2024-12-14 14:23:10 -0600

    [FIX] Ajuste incicializar componentes

--- Form1.cs
+++ Form1.cs
@@ -19,47 +19,10 @@ namespace MergePDF
         public Form1()
         {
             InitializeComponent();
-            InitializeControls();
+            
         }
 
-        private void InitializeControls()
-        {
-            // Create and configure folder selection button
-            Button btnSelectFolder = new Button();
-            btnSelectFolder.Text = "Select Folder";
-            btnSelectFolder.Location = new Point(20, 20);
-            btnSelectFolder.Size = new Size(120, 30);
-            btnSelectFolder.Click += BtnSelectFolder_Click;
-
-            // Create and configure merge button
-            Button btnMerge = new Button();
-            btnMerge.Text = "Merge PDFs";
-            btnMerge.Location = new Point(160, 20);
-            btnMerge.Size = new Size(120, 30);
-            btnMerge.Click += BtnMerge_Click;
-
-            // Create and configure status label
-            Label lblStatus = new Label();
-            lblStatus.Name = "lblStatus";
-            lblStatus.Location = new Point(20, 60);
-            lblStatus.Size = new Size(400, 20);
-            lblStatus.Text = "Select a folder containing PDF files";
-
-            // Create and configure list view for PDF files
-            ListView listView = new ListView();
-            listView.Name = "lvPDFs";
-            listView.Location = new Point(20, 90);
-            listView.Size = new Size(400, 200);
-            listView.View = View.Details;
-            listView.Columns.Add("PDF Files", 380);
-
-            // Add controls to form
-            this.Controls.AddRange(new Control[] { btnSelectFolder, btnMerge, lblStatus, listView });
-
-            // Configure form
-            this.Text = "PDF Merger";
-            this.Size = new Size(460, 350);
-        }
+       
 
         private void BtnSelectFolder_Click(object sender, EventArgs e)
         {
